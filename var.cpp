2e3db5ac86ca5bc3d1b71dfb9447fc79e6fbb62f
mylest913 <mylest913@gmail.com>
2024-07-21 16:14:13 -0700

    adding

---
+++ var.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The byte size of the variable char is " << sizeof(char) << endl;
+    cout << "The byte size of the variable int is " << sizeof(int) << endl;
+    cout << "The byte size of the variable long is " << sizeof(long) << endl;
+    cout << "The byte size of the variable long long is " << sizeof(long long) << endl;
+    cout << "The byte size of the variable double is " << sizeof(double) << endl;
+}
